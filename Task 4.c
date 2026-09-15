b6b7a079c587f2741237e9f02f3f95687915b5a7
AliRazzzza <alirazawork01@gmail.com>
2026-09-15 08:29:18 +0500

    task 4

---
+++ Task 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int lenght, width, area, perimeter;
+    printf("\nEnter lenght: ");
+    scanf("%d", & lenght);
+    printf("\nEnter width: ");
+    scanf("%d", & width);
+    area = lenght * widht;
+    perimeter = 2*(lenght + width);
+    printf("\nArea of the rectangle is %d", area);
+    printf("\nPerimeter of the rectangle is %d", perimeter);
+    return 0;
+}
