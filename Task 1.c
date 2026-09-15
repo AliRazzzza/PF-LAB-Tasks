8f5d2fc323bc668ac57eb446055b7f108f56f0e8
AliRazzzza <alirazawork01@gmail.com>
2026-09-15 08:25:53 +0500

    Task 1

---
+++ Task 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float marks, income;
+    printf("\nEnter your marks percentage: ");
+    scanf("%f", & marks);
+    printf("\nEnter your family income: ");
+    scanf("%f", & income);
+    if(marks >= 80 || income < 50000){
+        printf("\nYou have been qualified for scholarship");
+    }else{
+        printf("You did not quality for scholarship");
+    }
+    
+
+    return 0;
+}
